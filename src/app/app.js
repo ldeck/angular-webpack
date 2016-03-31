181231272871bc020c7b1dc44f1c97319461bfad
Lachlan Deck <lachlan.deck@gmail.com>
2016-03-31 16:51:24 +1100

    Expect ldeck/angular-webpack url

--- src/app/app.js
+++ src/app/app.js
@@ -7,12 +7,12 @@ let app = () => {
     template: require('./app.html'),
     controller: 'AppCtrl',
     controllerAs: 'app'
-  }
+  };
 };
 
 class AppCtrl {
   constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
+    this.url = 'https://github.com/ldeck/angular-webpack';
   }
 }
 
